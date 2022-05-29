10efa04fb051b89cb1ced919df7a7084f937ec55
Juliiis <jumartinnob@gmail.com>
2022-05-29 17:21:27 +0200

    method introducirRopa created

--- src/lavadora.js
+++ src/lavadora.js
@@ -4,5 +4,14 @@ var lavadora = {
     capacidad: 7,
     pesoTotal: 3,
     programaSeleccionado: "rapido",
-    programaEnProceso: true
+    programaEnProceso: true,
+
+    introducirRopa(){
+        const introducir = true
+        if(introducir === true){
+            return this.pesoTotal + 1
+        }
+    }
+
 }
+console.log(lavadora.introducirRopa(true))
