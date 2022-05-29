9caf79801d38bd85114052be18a794bb172b321e
Juliiis <jumartinnob@gmail.com>
2022-05-29 17:26:36 +0200

    method quitarRopa created

--- src/lavadora.js
+++ src/lavadora.js
@@ -11,7 +11,14 @@ var lavadora = {
         if(introducir === true){
             return this.pesoTotal + 1
         }
-    }
+    },
 
+    quitarRopa(){
+        const quitar = true
+        if(quitar === true){
+            return this.pesoTotal - 1
+        }
+    }
 }
 console.log(lavadora.introducirRopa(true))
+console.log(lavadora.quitarRopa(true))
